36af4fceecff7fc1298e6a7d7bddc86d0727613d
dibsan <un.sanbiters@gmail.com>
2026-03-12 16:46:23 +0530

    UC-3

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -1,7 +1,32 @@
-public class palindromecheckerapp {
-     public static void main(String[] args)
-     {
-        System.out.println("Welcome Palindrome Checker App");
-        System.out.println("Dibyansh Pandey");
-}
+public class UseCase2PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String input = "madam";
+
+        // Assume it is palindrome
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            // Compare characters from both ends
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
 }
